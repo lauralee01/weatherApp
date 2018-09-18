74e1282d670d66505550b1db6eada8974806b63c
sajicode <sajioloye@gmail.com>
2018-09-18 20:30:06 +0100

    weather search

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const yargs = require('yargs');
+/*const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode.js');
 
@@ -23,5 +23,22 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
 	}
 });
 
+// 0dbd91a0e13cc6635ac1a4bbb0b7f040
 
 
+
+*/
+
+const request = require('request');
+
+request({
+	url: 'https://api.darksky.net/forecast/0dbd91a0e13cc6635ac1a4bbb0b7f040/6.5243793,3.3792057',
+	json: true
+}, (error, response, body) => {
+	if (!error && response.statusCode === 200) {
+		console.log(`Temperature: ${body.currently.temperature}`);
+	} else {
+		console.log('Unable to fetch weather');
+	}
+	
+});
